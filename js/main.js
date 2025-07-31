6c9ccb931988c03d7ebf2c9dd81e4a6740c87929
anatoliy-nik <dessid@yandex.ru>
2025-07-31 18:00:35 +0300

    доделал 5-й Урок - Портфолио

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    // var mixer = mixitup('.container');
+    var mixer = mixitup('.portfolio__content');
+
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+      });
+});
