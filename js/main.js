7f4b87de09e69c2b4be3186d665ebea8f0a19096
anatoliy-nik <dessid@yandex.ru>
2025-08-10 17:25:10 +0300

    плавный скролл и липкий хэдер

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function () {
 
+  $(".menu a, .header__icon, .footer__top-link").on("click", function (e) {
+		//отменяем стандартную обработку нажатия по ссылке
+		e.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
@@ -14,12 +28,13 @@ $(function () {
         }
       }
     ]
+
+    
   });
 
-  $('.header__menu-btn').on('click', function () {
+  $('.header__menu-btn, .menu__list-link').on('click', function () {
     $('.menu').toggleClass('menu-active');
   });
-
 
   var mixer = mixitup('.portfolio__content');
   // плагин mixitup нужно ставить последним (это его баг)
